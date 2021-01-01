809af18ad5552c15bcfdb801d34baa168bd05560
athulatk <insinexatk10@cet.ac.in>
2021-01-01 13:04:15 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import {ToastContainer,toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; 
+import {Card,CardBody,Container,Col,Row} from 'reactstrap';
+import Icon from './components/Icon'
 
+const itemArray=new Array(9).fill("empty")
 function App() {
+
+  const [isCross,setIsCross]=useState(false);
+  const [winMessage,setWinMessage]=useState("");
+
+  const reloadGame = () =>{
+    setIsCross(false);
+    setWinMessage("");
+    itemArray.fill("empty",0,9);
+  }
+
+  const checkIsWinner = () =>{
+    if(itemArray[0]!=="empty" && itemArray[0]===itemArray[1] &&
+      itemArray[1]===itemArray[2]){
+        setWinMessage(`${itemArray[0]} wins!!`)
+      }
+    else if(itemArray[3]!=="empty" && itemArray[3]===itemArray[4] &&
+      itemArray[4]===itemArray[5]){
+      setWinMessage(`${itemArray[3]} wins!!`)
+      }
+      
+    else if(itemArray[6]!=="empty" && itemArray[6]===itemArray[7] &&
+      itemArray[7]===itemArray[8]){
+      setWinMessage(`${itemArray[6]} wins!!`)
+      } 
+    else if(itemArray[0]!=="empty" && itemArray[0]===itemArray[3] &&
+          itemArray[3]===itemArray[6]){
+          setWinMessage(`${itemArray[0]} wins!!`)
+          } 
+    else if(itemArray[1]!=="empty" && itemArray[1]===itemArray[4] &&
+      itemArray[4]===itemArray[7]){
+      setWinMessage(`${itemArray[1]} wins!!`)
+     } 
+    else if(itemArray[2]!=="empty" && itemArray[2]===itemArray[5] &&
+      itemArray[5]===itemArray[8]){
+      setWinMessage(`${itemArray[2]} wins!!`)
+      } 
+    else if(itemArray[0]!=="empty" && itemArray[0]===itemArray[4] &&
+      itemArray[4]===itemArray[8]){
+      setWinMessage(`${itemArray[0]} wins!!`)
+      } 
+    else if(itemArray[2]!=="empty" && itemArray[2]===itemArray[4] &&
+      itemArray[4]===itemArray[6]){
+      setWinMessage(`${itemArray[2]} wins!!`)
+      } 
+
+    }
+  const changeItem = itemNumber => {
+    if(winMessage){
+      return toast(winMessage,{type:"success"});
+    }
+    if(itemArray[itemNumber]==="empty"){
+        itemArray[itemNumber]=isCross ? "cross" : "circle"
+        setIsCross(!isCross);
+    }
+    else{
+      return toast("Already Filled",{type:"error"})
+    }
+    checkIsWinner();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Container className="p-5">
+       <ToastContainer position="bottom-center"/>
+       <Row>
+          <Col md={6} className="offset-md-3">
+          <h1 className="text-center">Tic-Tac-Toe</h1>
+            {winMessage ? (
+              <div className="mb-2 mt-2">
+                <h2 className="text-center" style={{color:'green'}}>
+                  {winMessage}
+                </h2>
+              </div>
+            ) : (
+              <h2 className="text-center">
+                {isCross ? "Cross Turn" : "Circle Turn" }
+              </h2>
+            )}
+            <div className="grid">
+              {itemArray.map((item,index)=>(
+                <Card onClick={()=>changeItem(index)}>
+                  <CardBody className="box">
+                    <Icon name={item}/>
+                  </CardBody>
+                </Card>
+              ))}
+            </div>
+            {winMessage ? (
+              <button className="reload" onClick={reloadGame}>
+              Reload Game
+              </button>
+            ):""}
+            
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
-}
+            }
 
 export default App;
